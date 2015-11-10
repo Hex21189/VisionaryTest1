dcaafe98c7eef04539e440138230edffc31351a6
Hex21189 <Hex21189@msn.com>
2015-11-09 21:47:48 -0800

    forgot to recheck in files with their new names

---
+++ Assets/C_Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Logic for controlling spawned bullets.
+/// </summary>
+public class Bullet : MonoBehaviour
+{
+    private Transform myTransform;
+
+	/// <summary>
+	/// Initialize bullet in random direction.
+	/// </summary>
+	protected void Start()
+    {
+        myTransform = transform;
+        myTransform.eulerAngles = new Vector3(myTransform.eulerAngles.z, myTransform.eulerAngles.y, Random.Range(-45, 45));
+	}
+	
+	/// <summary>
+	/// Move this bullet forward.
+    /// TODO: make speed adjustable.
+    /// TODO: seperate bullet logic for more interesting bullet movement models.
+	/// </summary>
+	protected void Update()
+    {
+        myTransform.position += myTransform.right * 10 * Time.fixedDeltaTime; // here
+	}
+
+    /// <summary>
+    /// Destory this bullet when it collides with an enemy.
+    /// </summary>
+    /// <param name="collider">Enemy collider.</param>
+    protected void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/C_Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Script for controlling enemies AI and stats.
+// TODO: seperate this into multiple parts for easy enemy creation
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    private Transform myTransform; // Cached transform for quick look ups in the update loop.
+    private float randomOffset; // TODO: looks unnecessary
+
+    /// <summary>
+    /// Load initial object settings.
+    /// </summary>
+    protected void Start()
+    {
+        myTransform = transform;
+        randomOffset = Random.Range(0f, 1f);
+	}
+	
+	/// <summary>
+	/// Move this Enemy south west.
+    /// TODO: Seperate movement logic.
+    /// TODO: Make the direction configurable and not fixed so it the movement logic can be used for multiple enemies.
+	/// </summary>
+	protected void Update()
+    {
+        myTransform.position -= myTransform.right * 3 * Time.deltaTime;
+        myTransform.position += Mathf.Sign(Time.time + randomOffset) * Vector3.down * Time.deltaTime;
+	}
+
+    /// <summary>
+    /// Desotry this enemy when it collides with a bullet object.
+    /// </summary>
+    /// <param name="collider">Bullet collider.</param>
+    protected void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
